a2bbc15238ea8ef90e992a76e01612b9663fecbc
Jan Luca Binder <janluca.binder@hotmail.de>
2020-06-03 20:46:52 +0200

    added color and renderer cpp file

---
+++ Renderer/src/core/general/Color.cpp
@@ -0,0 +1,15 @@
+#include "../GRenderer.h"
+
+GGeneral::Color::Color(byte r, byte g, byte b) : red(r), green(g), blue(b) {
+	
+}
+
+byte GGeneral::Color::operator[](byte i) {
+	switch (i)
+	{
+	case 0: return red;
+	case 1: return green;
+	case 2: return blue;
+	default: return 0;
+	}
+}
---
+++ Renderer/src/core/renderer/Renderer.cpp
@@ -0,0 +1,16 @@
+#include "../GRenderer.h"
+#include <GL/glew.h>
+
+std::string GRenderer::getCurentOpenGLVersion() {
+	auto c = glGetString(GL_VERSION);
+	std::string r;
+	for (unsigned int i = 0; c[i] != '\0'; i++) {
+		r += c[i];
+	}
+	return r;
+}
+
+void GRenderer::clear(GGeneral::Color color) {
+	glClearColor(((float)color[0]) / 255, ((float)color[1]) / 255, ((float)color[2]) / 255, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
